08f4c73ae2f0cb4dfefc3b47d4f45efe0c4bcfc0
Eduardo Ortiz <leortiz@espol.edu.ec>
2020-07-01 15:02:26 -0500

    [AL-00] Neighborhood database

--- neighborhood/models.py
+++ neighborhood/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Person(models.Model):
+    person_id = models.CharField(max_length=200)
+    id_card = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=50)
+    second_name = models.CharField(max_length=50)
+    status = models.CharField(max_length=4)
+    created_at = models.DateTimeField(default=timezone.now)
---
+++ neighborhood_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Eduardo\Sof2\open-neighborhood-backend\neighborhood_env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
