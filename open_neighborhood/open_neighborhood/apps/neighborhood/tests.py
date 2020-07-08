5e92fca6c0378d627ff82711d3b81fd2d9a136fd
Eduardo Ortiz H <leortiz@espol.edu.ec>
2020-07-07 19:24:21 -0500

    [AL-00] Test

--- open_neighborhood/open_neighborhood/apps/neighborhood/tests.py
+++ open_neighborhood/open_neighborhood/apps/neighborhood/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
-
+import json
+from graphene_django.utils.testing import GraphQLTestCase
+import schema
 # Create your tests here.
+
+class MyTestCase(GraphQLTestCase):
+    GRAPHQL_SCHEMA = schema
+
+    def test_some_query(self):
+        response = self.query(
+            '''
+            query{
+                Person{
+                    person_id
+                    id_card
+                    name
+                    birth
+                    status
+                    created_at 
+                }
+            }                     
+            ''',
+            op_name='Person'
+        )
+
+        content = json.loads(response.content)
+
+        # This validates the status code and if you get errors
+        self.assertResponseNoErrors(response)
+
+        # Add some more asserts if you like
